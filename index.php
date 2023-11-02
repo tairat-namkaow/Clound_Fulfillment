f862124665d43d155302498f241527e8924f567c
Pattaraphon D <pattaraphon.dch@gmail.com>
2023-11-02 21:39:02 +0700

    Update index.php
    update logged in as : firstname + lastname

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Start Bootstrap
+                    <?php echo $result_Shop[1]." ". $result_Shop[3];?>
                 </div>
             </nav>
         </div>
