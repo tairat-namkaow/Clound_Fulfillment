cc67add9272848f4ddcdfda762cefc7f8f81cb29
tairat-namkaow <tairat.namkaow@gmail.com>
2023-11-22 23:32:39 +0700

    แก้

--- index.php
+++ index.php
@@ -132,7 +132,10 @@ $result_shop = mysqli_fetch_array($query_shop);
                         <div class="col-xl-3 col-md-6">
                             <div class="card bg-success text-white mb-4">
                                 <?php
-                                $sql_sku = "SELECT count(*) as sku FROM product
+                                $sql_sku = "SELECT COUNT(DISTINCT product.Product_name) AS distinct_count
+                                FROM product_detail
+                                INNER JOIN shop ON product_detail.Shop_id = shop.Shop_id
+                                INNER JOIN product ON product_detail.Product_id = product.Product_id                                
                                 WHERE shop.Shop_email = '" . $_SESSION['Shop_email'] . "'";
                                 $query_sku = mysqli_query($Connection, $sql_sku);
                                 $result_sku = mysqli_fetch_array($query_sku);
