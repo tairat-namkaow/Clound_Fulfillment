3b5cf59aea7cc932af8f3bb2e7c7326c99521e2b
Pattaraphon D <pattaraphon.dch@gmail.com>
2023-11-24 16:37:25 +0700

    export

--- shop_export_outdata.php
+++ shop_export_outdata.php
@@ -18,16 +18,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     product_category.Category_name,
     detail.Detail_quantity,
     shop.Shop_name,
-    warehouse.Warehouse_zone,
     warehouse.Location,
-    product_detail.Product_time_add
+    order_main.Order_address,
+    detail.Detail_date
 FROM product_detail
 INNER JOIN product ON product.Product_id = product_detail.Product_id
 INNER JOIN shop ON product_detail.Shop_id = shop.Shop_id
 INNER JOIN product_category ON product_category.Category_id = product.Category_id
 INNER JOIN warehouse ON product_detail.Warehouse_id = warehouse.Warehouse_id
 INNER JOIN detail ON detail.Product_detail_id = product_detail.Product_detail_id
-WHERE shop.Shop_email = '" . $_SESSION['Shop_email'] . "' and product_detail.Product_time_add BETWEEN ? AND ?
+INNER JOIN order_main ON detail.Order_id = order_main.Order_id
+WHERE shop.Shop_email = '" . $_SESSION['Shop_email'] . "' and detail.Detail_date BETWEEN ? AND ?
 ORDER BY product.Product_id";
 
     // Prepare and bind the statement
@@ -49,7 +50,7 @@ ORDER BY product.Product_id";
         $file = fopen('php://output', 'w');
 
         // Write headers to the CSV file
-        $header = array('Product_id', 'Product_name', 'Product_category', 'Out-Quantity', 'Shop_name', 'Warehouse_zone', 'Location', 'Withdraw Date');
+        $header = array('Product_id', 'Product_name', 'Product_category', 'Out-Quantity', 'Shop_name', 'Warehouse_location', 'Order_address', 'Withdraw Date');
         fputcsv($file, $header);
 
         // Fetch and write data to the CSV file
